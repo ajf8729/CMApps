f05be1adce98121efc9a7606675c179fd7b9695b
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-11-14 04:43:47 -0500

    Added files for Firefox ESR

---
+++ Firefox ESR/91.3.0/x64/Install.cmd
@@ -0,0 +1,2 @@
+@echo off
+"%~dp0Firefox Setup 91.3.0esr.exe" /S /DesktopShortcut=false
---
+++ Firefox ESR/build.ps1
@@ -0,0 +1,21 @@
+$Parameters = @{
+    'ContentRootPath'            = '\\corp.ajf8729.com\Shares\Source\APPS';
+    'InstallScriptFilename'      = 'Install.cmd';
+    'SiteCode'                   = 'PS1';
+    'UserLimitingCollectionId'   = 'SMS00002';
+    'DeviceLimitingCollectionId' = 'SMS00001';
+    'Type'                       = 'EXE';
+    'Publisher'                  = 'Mozilla';
+    'Name'                       = 'Firefox ESR';
+    'Architecture'               = 'x64';
+    'RegKeyName'                 = 'Mozilla Firefox*ESR (x64 en-US)'
+    'RegKeyContainsVersion'      = $false;
+    'Is64bit'                    = $true;
+    'EstimatedRuntimeMins'       = '5';
+    'MaximumRuntimeMins'         = '15';
+    'DPGroupName'                = 'ALL';
+    'IsFreeApp'                  = $true;
+    'Verbose'                    = $true
+}
+
+.\New-Application.ps1 @Parameters
