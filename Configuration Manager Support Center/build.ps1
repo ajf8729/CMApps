2f22d8dab70e1ed659494c90bf5338d21c612af3
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-11-21 20:06:08 -0500

    Added build scripts for 7-Zip, CM Support Center, NP++, PS, and VSC

---
+++ Configuration Manager Support Center/build.ps1
@@ -0,0 +1,19 @@
+$Parameters = @{
+    'ContentRootPath'            = '\\corp.ajf8729.com\Shares\Source\APPS';
+    'InstallScriptFilename'      = 'Install.cmd';
+    'SiteCode'                   = 'PS1';
+    'UserLimitingCollectionId'   = 'SMS00002';
+    'DeviceLimitingCollectionId' = 'SMS00001';
+    'Type'                       = 'MSI';
+    'Publisher'                  = 'Microsoft';
+    'Name'                       = 'Configuration Manager Support Center';
+    'Architecture'               = 'x64';
+    'Is64bit'                    = $true;
+    'EstimatedRuntimeMins'       = '5';
+    'MaximumRuntimeMins'         = '15';
+    'DPGroupName'                = 'ALL';
+    'IsFreeApp'                  = $true;
+    'Verbose'                    = $true
+}
+
+.\New-Application.ps1 @Parameters
---
+++ Visual Studio Code/build.ps1
@@ -0,0 +1,21 @@
+$Parameters = @{
+    'ContentRootPath'            = '\\corp.ajf8729.com\Shares\Source\APPS';
+    'InstallScriptFilename'      = 'Install.cmd';
+    'SiteCode'                   = 'PS1';
+    'UserLimitingCollectionId'   = 'SMS00002';
+    'DeviceLimitingCollectionId' = 'SMS00001';
+    'Type'                       = 'EXE';
+    'Publisher'                  = 'Microsoft';
+    'Name'                       = 'Visual Studio Code';
+    'Architecture'               = 'x64';
+    'RegKeyName'                 = '{EA457B21-F73E-494C-ACAB-524FDE069978}_is1'
+    'RegKeyContainsVersion'      = $false;
+    'Is64bit'                    = $true;
+    'EstimatedRuntimeMins'       = '5';
+    'MaximumRuntimeMins'         = '15';
+    'DPGroupName'                = 'ALL';
+    'IsFreeApp'                  = $true;
+    'Verbose'                    = $true
+}
+
+.\New-Application.ps1 @Parameters
